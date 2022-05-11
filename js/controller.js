699de9863c3b32b6c7ed20a746ee9a85058fe874
ponzukun <p1o1n1z1u@gmail.com>
2022-05-11 18:57:30 +0900

    [change name] advanceGame -> advanceDate

--- js/controller.js
+++ js/controller.js
@@ -12,11 +12,11 @@ export class Controller {
 
             document.getElementById("login-page").innerHTML = "";
             document.getElementById("main-page").append(View.createMainPage(user));
-            Controller.advanceGame(user);
+            Controller.advanceDate(user);
         });
     }
 
-    static advanceGame(user) {
+    static advanceDate(user) {
         setInterval(function(){
             user.spentDays++;
             document.getElementById("spent-days").innerHTML = `${user.spentDays} days`;
